3dab08eaab6172e5baa3f330316a93032ef83d70
Pavel Solokha <pavel.solokha@gmail.com>
2019-09-28 13:02:04 +0300

    version 1.0

--- src/main/java/GetRequest.java
+++ src/main/java/GetRequest.java
@@ -1,6 +1,4 @@
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
